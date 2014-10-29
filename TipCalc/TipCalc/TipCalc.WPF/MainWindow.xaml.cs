0ac2ed26d9911b14f28d6237049368246082bf42
Jonkeda <jonkeda@hotmail.com>
2014-10-29 16:51:03 +0100

    Added tipcalc for WPF

---
+++ TipCalc/TipCalc/TipCalc.WPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Wpf;
+
+
+namespace TipCalc.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+
+            Platform platform = TipCalc.App.GetMainPage().GetPlatform(this); ;
+
+            cp.Content = (UIElement)platform;
+        }
+    }
+}
